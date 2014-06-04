a2e8f7088e4bf34e23f8e1893da1268d1ff82b8e
Henri Wiechers <hwiechers@gmail.com>
2014-06-04 22:34:49 +0200

    Intial import

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var output = new [] {
+                "###################" + Environment.NewLine +
+                "#........#........#" + Environment.NewLine +
+                "#*##.###.#.###.##*#" + Environment.NewLine +
+                "#.##.###.#.###.##.#" + Environment.NewLine +
+                "#.................#" + Environment.NewLine +
+                "#.##.#.#####.#.##.#" + Environment.NewLine +
+                "#....#...#...#....#" + Environment.NewLine +
+                "####.###.#.###.####" + Environment.NewLine +
+                "####.#.......#.####" + Environment.NewLine +
+                "####.#.## ##.#.####" + Environment.NewLine +
+                "   !A..#   #...B   " + Environment.NewLine +
+                "####.#.## ##.#.####" + Environment.NewLine +
+                "####.#.......#.####" + Environment.NewLine +
+                "####.#.#####.#.####" + Environment.NewLine +
+                "#........#........#" + Environment.NewLine +
+                "#.##.###.#.###.##.#" + Environment.NewLine +
+                "#*.#...........#.*#" + Environment.NewLine +
+                "##.#.#.#####.#.#.##" + Environment.NewLine +
+                "#....#...#...#....#" + Environment.NewLine +
+                "#.######.#.######.#" + Environment.NewLine +
+                "#.................#" + Environment.NewLine +
+                "###################",
+
+                "###################" + Environment.NewLine +
+                "#........#........#" + Environment.NewLine +
+                "#*##.###.#.###.##*#" + Environment.NewLine +
+                "#.##.###.#.###.##.#" + Environment.NewLine +
+                "#.................#" + Environment.NewLine +
+                "#.##.#.#####.#.##.#" + Environment.NewLine +
+                "#....#...#...#....#" + Environment.NewLine +
+                "####.###.#.###.####" + Environment.NewLine +
+                "####.#.......#.####" + Environment.NewLine +
+                "####.#.## ##.#.####" + Environment.NewLine +
+                "   !B..#   #..A!   " + Environment.NewLine +
+                "####.#.## ##.#.####" + Environment.NewLine +
+                "####.#.......#.####" + Environment.NewLine +
+                "####.#.#####.#.####" + Environment.NewLine +
+                "#........#........#" + Environment.NewLine +
+                "#.##.###.#.###.##.#" + Environment.NewLine +
+                "#*.#...........#.*#" + Environment.NewLine +
+                "##.#.#.#####.#.#.##" + Environment.NewLine +
+                "#....#...#...#....#" + Environment.NewLine +
+                "#.######.#.######.#" + Environment.NewLine +
+                "#.................#" + Environment.NewLine +
+                "###################",
+
+                "###################" + Environment.NewLine +
+                "#........#........#" + Environment.NewLine +
+                "#*##.###.#.###.##*#" + Environment.NewLine +
+                "#.##.###.#.###.##.#" + Environment.NewLine +
+                "#.................#" + Environment.NewLine +
+                "#.##.#.#####.#.##.#" + Environment.NewLine +
+                "#....#...#...#....#" + Environment.NewLine +
+                "####.###.#.###.####" + Environment.NewLine +
+                "####.#.......#.####" + Environment.NewLine +
+                "####.#.## ##.#.####" + Environment.NewLine +
+                "   A ..#   #..B!   " + Environment.NewLine +
+                "####.#.## ##.#.####" + Environment.NewLine +
+                "####.#.......#.####" + Environment.NewLine +
+                "####.#.#####.#.####" + Environment.NewLine +
+                "#........#........#" + Environment.NewLine +
+                "#.##.###.#.###.##.#" + Environment.NewLine +
+                "#*.#...........#.*#" + Environment.NewLine +
+                "##.#.#.#####.#.#.##" + Environment.NewLine +
+                "#....#...#...#....#" + Environment.NewLine +
+                "#.######.#.######.#" + Environment.NewLine +
+                "#.................#" + Environment.NewLine +
+                "###################",
+
+                "###################" + Environment.NewLine +
+                "#........#........#" + Environment.NewLine +
+                "#*##.###.#.###.##*#" + Environment.NewLine +
+                "#.##.###.#.###.##.#" + Environment.NewLine +
+                "#.................#" + Environment.NewLine +
+                "#.##.#.#####.#.##.#" + Environment.NewLine +
+                "#....#...#...#....#" + Environment.NewLine +
+                "####.###.#.###.####" + Environment.NewLine +
+                "####.#.......#.####" + Environment.NewLine +
+                "####.#.## ##.#.####" + Environment.NewLine +
+                "     ..# B #.. A   " + Environment.NewLine +
+                "####.#.## ##.#.####" + Environment.NewLine +
+                "####.#.......#.####" + Environment.NewLine +
+                "####.#.#####.#.####" + Environment.NewLine +
+                "#........#........#" + Environment.NewLine +
+                "#.##.###.#.###.##.#" + Environment.NewLine +
+                "#*.#...........#.*#" + Environment.NewLine +
+                "##.#.#.#####.#.#.##" + Environment.NewLine +
+                "#....#...#...#....#" + Environment.NewLine +
+                "#.######.#.######.#" + Environment.NewLine +
+                "#.................#" + Environment.NewLine +
+                "###################",
+
+                "###################" + Environment.NewLine +
+                "#........#........#" + Environment.NewLine +
+                "#*##.###.#.###.##*#" + Environment.NewLine +
+                "#.##.###.#.###.##.#" + Environment.NewLine +
+                "#.................#" + Environment.NewLine +
+                "#.##.#.#####.#.##.#" + Environment.NewLine +
+                "#....#...#...#....#" + Environment.NewLine +
+                "####.###.#.###.####" + Environment.NewLine +
+                "####.#.......#.####" + Environment.NewLine +
+                "####.#.## ##.#.####" + Environment.NewLine +
+                "     ..#   #..     " + Environment.NewLine +
+                "####.#.##A##.#.####" + Environment.NewLine +
+                "####.#.......#.####" + Environment.NewLine +
+                "####.#.#####.#.####" + Environment.NewLine +
+                "#........#........#" + Environment.NewLine +
+                "#.##.###.#.###.##.#" + Environment.NewLine +
+                "#*.#...........#.*#" + Environment.NewLine +
+                "##.#.#.#####.#.#.##" + Environment.NewLine +
+                "#....#...#...#....#" + Environment.NewLine +
+                "#.######.#.######.#" + Environment.NewLine +
+                "#.................#" + Environment.NewLine +
+                "###################",
+
+                "###################" + Environment.NewLine +
+                "#........#........#" + Environment.NewLine +
+                "#*##.###.#.###.##*#" + Environment.NewLine +
+                "#.##.###.#.###.##.#" + Environment.NewLine +
+                "#.................#" + Environment.NewLine +
+                "#.##.#.#####.#.##.#" + Environment.NewLine +
+                "#....#...#...#....#" + Environment.NewLine +
+                "####.###.#.###.####" + Environment.NewLine +
+                "####.#.......#.####" + Environment.NewLine +
+                "####.#.##A##.#.####" + Environment.NewLine +
+                "     ..#   #..     " + Environment.NewLine +
+                "####.#.##B##.#.####" + Environment.NewLine +
+                "####.#.......#.####" + Environment.NewLine +
+                "####.#.#####.#.####" + Environment.NewLine +
+                "#........#........#" + Environment.NewLine +
+                "#.##.###.#.###.##.#" + Environment.NewLine +
+                "#*.#...........#.*#" + Environment.NewLine +
+                "##.#.#.#####.#.#.##" + Environment.NewLine +
+                "#....#...#...#....#" + Environment.NewLine +
+                "#.######.#.######.#" + Environment.NewLine +
+                "#.................#" + Environment.NewLine +
+                "###################",
+
+                
+                "###################" + Environment.NewLine +
+                "#........#........#" + Environment.NewLine +
+                "#*##.###.#.###.##*#" + Environment.NewLine +
+                "#.##.###.#.###.##.#" + Environment.NewLine +
+                "#.................#" + Environment.NewLine +
+                "#.##.#.#####.#.##.#" + Environment.NewLine +
+                "#....#...#...#....#" + Environment.NewLine +
+                "####.###.#.###.####" + Environment.NewLine +
+                "####.#.......#.####" + Environment.NewLine +
+                "####.#.##B##.#.####" + Environment.NewLine +
+                "     ..#   #..     " + Environment.NewLine +
+                "####.#.## ##.#.####" + Environment.NewLine +
+                "####.#...A...#.####" + Environment.NewLine +
+                "####.#.#####.#.####" + Environment.NewLine +
+                "#........#........#" + Environment.NewLine +
+                "#.##.###.#.###.##.#" + Environment.NewLine +
+                "#*.#...........#.*#" + Environment.NewLine +
+                "##.#.#.#####.#.#.##" + Environment.NewLine +
+                "#....#...#...#....#" + Environment.NewLine +
+                "#.######.#.######.#" + Environment.NewLine +
+                "#.................#" + Environment.NewLine +
+                "###################",
+
+                
+                "###################" + Environment.NewLine +
+                "#........#........#" + Environment.NewLine +
+                "#*##.###.#.###.##*#" + Environment.NewLine +
+                "#.##.###.#.###.##.#" + Environment.NewLine +
+                "#.................#" + Environment.NewLine +
+                "#.##.#.#####.#.##.#" + Environment.NewLine +
+                "#....#...#...#....#" + Environment.NewLine +
+                "####.###.#.###.####" + Environment.NewLine +
+                "####.#...A...#.####" + Environment.NewLine +
+                "####.#.## ##.#.####" + Environment.NewLine +
+                "     ..#   #..     " + Environment.NewLine +
+                "####.#.## ##.#.####" + Environment.NewLine +
+                "####.#...B...#.####" + Environment.NewLine +
+                "####.#.#####.#.####" + Environment.NewLine +
+                "#........#........#" + Environment.NewLine +
+                "#.##.###.#.###.##.#" + Environment.NewLine +
+                "#*.#...........#.*#" + Environment.NewLine +
+                "##.#.#.#####.#.#.##" + Environment.NewLine +
+                "#....#...#...#....#" + Environment.NewLine +
+                "#.######.#.######.#" + Environment.NewLine +
+                "#.................#" + Environment.NewLine +
+                "###################",
+            };
+
+            var ply = 0;
+            if (File.Exists("ply.txt"))
+            {
+                ply = int.Parse(File.ReadAllText("ply.txt"));
+            }
+
+            if (ply >= output.Length)
+            {
+                File.Delete("ply.txt");
+                return;
+            }
+
+            File.WriteAllText("ply.txt", (ply + 1).ToString());
+
+            File.WriteAllText("game.state", output[ply]);
+        }
+    }
+}
